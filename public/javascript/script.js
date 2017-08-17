127accdad69070576afb74e84f2d9c2913a01d76
SuperSpaceZova <ravalico.giovanni@gmail.com>
2017-08-17 17:33:55 +0200

    giovanni - wk6 day4 - wk7_petition - NOTHING TO SEE - partial commit

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,3 +1,35 @@
-$(document).ready(function() {
-    $('main > p').html('petition body');
-});
+$( document ).ready( function () {
+
+
+    // DOM HANDLERS:
+
+    var $canvas = $( '#canvas' );
+    var $canvasInput = $( '#canvasInput' );
+
+    $canvas.mouseenter( function () {
+        console.log( 'mouse enter' );
+        listenMouseMove();
+    } );
+
+    $canvas.mouseleave( function () {
+        $canvas.off( 'mousemove', console.log( 'mouse leave' ) );
+    } );
+
+
+    function listenMouseMove() {
+        $canvas.mousemove( function ( event ) {
+            var mouseX = event.offsetX;
+            var mouseY = event.offsetY;
+            console.log( mouseX, mouseY );
+        } );
+    }
+
+
+
+
+
+
+
+
+
+} );
