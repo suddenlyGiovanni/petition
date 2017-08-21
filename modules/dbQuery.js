f6d3015836a4471fc256b25ef929f41db056a0f3
SuperSpaceZova <ravalico.giovanni@gmail.com>
2017-08-21 22:48:04 +0200

    giovanni - wk7 day1 - wk7_petition4 - bcrypt implementation DONE/Applications/Postgres.app/Contents/Versions/9.6/bin/psql -p5432 -d places ;

--- modules/dbQuery.js
+++ modules/dbQuery.js
@@ -2,15 +2,88 @@
 
 // REQUIRED MODULES_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
 const spicedPg = require( 'spiced-pg' );
+
 const {
     dbUser,
     dbPass,
     dbName
 } = require( '../config/secrets.json' );
 
+const {
+    hashPassword,
+    checkPassword
+} = require( './hasher' );
 
 // MODULES VARIABLES_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
 const db = spicedPg( `postgres:${dbUser}:${dbPass}@localhost:5432/${dbName}` );
+
+// CREATE NEW USER
+const postUser = ( firstName, lastName, email, password ) => {
+    // hash user password with bcrypt ( hashPassword ) before saving
+    return hashPassword( password ).then( ( hashedPass ) => {
+        const query = 'INSERT INTO users ( "firstName", "lastName", email, password) VALUES ($1, $2, $3, $4) RETURNING id, "firstName", "lastName"';
+        return db.query( query, [
+            firstName,
+            lastName,
+            email,
+            hashedPass
+        ] );
+    } ).then( ( userSession ) => {
+        return {
+            user_id: userSession.rows[ 0 ].id,
+            firstName: userSession.rows[ 0 ].firstName,
+            lastName: userSession.rows[ 0 ].lastName
+        };
+    } ).catch( ( err ) => {
+        console.error( err.stack );
+    } );
+};
+
+
+// AUTHENTICATE USER
+const checkUser = ( email, password ) => {
+    // step 1 - search on db for matching email.
+    const query = 'SELECT id, "firstName", "lastName", email, password FROM users WHERE email = $1';
+    return db.query( query, [ email ] ).then( ( dbUser ) => {
+        return {
+            id: dbUser.rows[ 0 ].id,
+            firstName: dbUser.rows[ 0 ].firstName,
+            lastName: dbUser.rows[ 0 ].lastName,
+            email: dbUser.rows[ 0 ].email,
+            hashedPass: dbUser.rows[ 0 ].password
+        };
+    } ).then( ( dbUser ) => {
+        // step 2 - convert provided password and checkPassword
+        // step 3 - checkPassword returns either true or false.
+        return checkPassword(password, dbUser.hashedPass).then( (doesMatch) => {
+            if (!doesMatch) {
+                throw 'wrong email and password';
+            }
+            return {
+                user_id: dbUser.id,
+                firstName: dbUser.firstName,
+                lastName: dbUser.lastName,
+            };
+        });
+    } ).catch( ( err ) => {
+        console.error( err.stack );
+    } );
+};
+
+// ADD NEW PROFILE to A USER
+const postUserProfile = ( user_id, age, city, url ) => {
+    const query = 'INSERT INTO user_profiles (user_id, age, city, url) VALUES ($1, $2, $3, $4)';
+    return db.query( query, [
+        user_id,
+        age,
+        city,
+        url
+    ] ).then( () => {
+        return;
+    } ).catch( ( err ) => {
+        console.error( err.stack );
+    } );
+};
 
 
 // save new signature to DB
@@ -29,7 +102,7 @@ const postSignature = ( user_id, signature ) => {
 
 // retrieve specified signature form DB
 const getSignature = ( user_id ) => {
-    return db.query( `SELECT signature FROM signatures WHERE id = $1`, [ user_id ] ).then( ( results ) => {
+    return db.query( 'SELECT signature FROM signatures WHERE id = $1', [ user_id ] ).then( ( results ) => {
         return results.rows[ 0 ].signature;
     } ).catch( ( err ) => {
         console.error( err.stack );
@@ -50,7 +123,7 @@ const getSigners = () => {
     } );
 };
 
-// retrive all signer from a specified city
+// retrieve all signer from a specified city
 
 const getSignersCity = ( city ) => {
     const query = `SELECT users."firstName", users."lastName", user_profiles.age, user_profiles.url
@@ -64,69 +137,6 @@ const getSignersCity = ( city ) => {
         console.error( err.stack );
     } );
 };
-
-// save new user to DB
-const postUser = ( firstName, lastName, email, password ) => {
-    const query = `INSERT INTO users ( "firstName", "lastName", email, password) VALUES ($1, $2, $3, $4) RETURNING id, "firstName", "lastName"`;
-    return db.query( query, [
-        firstName,
-        lastName,
-        email,
-        password
-    ] ).then( ( userSession ) => {
-        return {
-            user_id: userSession.rows[ 0 ].id,
-            firstName: userSession.rows[ 0 ].firstName,
-            lastName: userSession.rows[ 0 ].lastName
-        };
-    } ).catch( ( err ) => {
-        console.error( err.stack );
-    } );
-};
-
-// save new profile to DB
-
-const postUserProfile = ( user_id, age, city, url ) => {
-    const query = 'INSERT INTO user_profiles (user_id, age, city, url) VALUES ($1, $2, $3, $4)';
-    return db.query( query, [
-        user_id,
-        age,
-        city,
-        url
-    ] ).then( () => {
-        return;
-    } ).catch( ( err ) => {
-        console.error( err.stack );
-    } );
-};
-
-// authenticate user
-const checkUser = ( email, password ) => {
-    const query = `SELECT id, "firstName", "lastName", email, password FROM users WHERE email = $1`;
-    return db.query( query, [ email ] ).then( ( dbUser ) => {
-        // console.log( dbUser.rows[ 0 ] );
-        return {
-            id: dbUser.rows[ 0 ].id,
-            firstName: dbUser.rows[ 0 ].firstName,
-            lastName: dbUser.rows[ 0 ].lastName,
-            email: dbUser.rows[ 0 ].email,
-            password: dbUser.rows[ 0 ].password
-        };
-    } ).then( ( dbUser ) => {
-        if ( dbUser.password === password ) {
-            return {
-                user_id: dbUser.id,
-                firstName: dbUser.firstName,
-                lastName: dbUser.lastName,
-            };
-        } else {
-            throw 'password do not match';
-        }
-    } ).catch( ( err ) => {
-        console.error( err.stack );
-    } );
-};
-
 
 module.exports.postUser = postUser;
 module.exports.postUserProfile = postUserProfile;
